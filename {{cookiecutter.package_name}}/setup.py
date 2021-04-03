918f9cc0a92e89173bcfb67c9bc5c816f0caa804
Pierpaolo Da Fieno <pierpaolo.dafieno@gmail.com>
2021-04-03 12:49:22 +0200

    Changed for local packages

--- {{cookiecutter.package_name}}/setup.py
+++ {{cookiecutter.package_name}}/setup.py
@@ -16,7 +16,6 @@ def read(filename):
 setup(
     name="{{ cookiecutter.package_name }}",
     version="{{ cookiecutter.package_version }}",
-    url="{{ cookiecutter.package_url }}",
     license='MIT',
 
     author="{{ cookiecutter.author_name }}",
